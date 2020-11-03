ace11cc5f321e33f969d731f86fc4141a7971ee5
Ayoze <ayoze.mesa102@alu.ulpgc.es>
2020-11-03 19:18:37 +0000

    v1

--- src/main/java/com/mycompany/practica5/Lienzo.java
+++ src/main/java/com/mycompany/practica5/Lienzo.java
@@ -1,12 +1,9 @@
 package com.mycompany.practica5;
 
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,21 +16,46 @@ import javax.swing.JPanel;
  */
 public class Lienzo extends JPanel {
     private BufferedImage imagen = null;
+    private BufferedImage logo = null;
 
     public Lienzo() {
         try {
-            imagen = ImageIO.read(new URL ("https://quecartucho.es/blog/wp-content/uploads/2014/05/rgb.jpg"));
-            this.setPreferredSize(new Dimension(imagen.getWidth(), imagen.getHeight()));
+            imagen = ImageIO.read(new URL ("https://i.ibb.co/mCgxYWH/rgb.jpg"));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Lienzo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Lienzo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            logo = ImageIO.read(new URL ("https://i.ibb.co/1vv3cx6/logo.png"));
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Lienzo.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(Lienzo.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+    public BufferedImage getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(BufferedImage imagen) {
+        this.imagen = imagen;
+    }
+
+    public BufferedImage getLogo() {
+        return logo;
+    }
+
+    public void setLogo(BufferedImage logo) {
+        this.logo = logo;
+    }
     
     @Override
-    public void paintComponents(Graphics g) {
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(imagen, 0, 0, null);
+        g.drawImage(logo, 0, 0, null);
     }
 }
